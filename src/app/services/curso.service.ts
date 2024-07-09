56eb501689340264447514467d346ed35c13ee6e
AlfonsoAguor <56021919+AlfonsoAguor@users.noreply.github.com>
2024-07-09 09:42:41 +0200

    Update curso.service.ts

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Curso } from '../models/curso';
 
-
 @Injectable()
 export class CursoService {
-	public cursos:Array<Curso>;
+	public cursos: Array<Curso>;
 
 	constructor(){
-		this.cursos=[
-			new Curso('Curso de angular 15',15, 'finalizado', 'assets/img/angular.png'),
-			new Curso('Master en JavaScript',33, 'finalizado', 'assets/img/masterJavaScript.png'),
-			new Curso('Master en FrameWorks JavaScript',28, 'en curso', ''),
-      new Curso('Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap',21, 'en curso', ''),
-      }
-		]
-	getCursos(): Array<Curso>{
+		this.cursos = [
+			new Curso('Curso de angular 15', 15, 'finalizado', 'assets/img/angular.png'),
+			new Curso('Master en JavaScript', 33, 'finalizado', 'assets/img/masterJavaScript.png'),
+			new Curso('Master en FrameWorks JavaScript', 28, 'en curso', ''),
+			new Curso('Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap', 21, 'en curso', '')
+		];
+	}
+
+	getCursos(): Array<Curso> {
 		return this.cursos;
 	}
 }
