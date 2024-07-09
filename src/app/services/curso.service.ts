6260a8e1d096a0547786dc82d4011dbbeb5045f8
AlfonsoAguor <56021919+AlfonsoAguor@users.noreply.github.com>
2024-07-09 09:29:54 +0200

    Update curso.service.ts
    Cambios en las horas del curso

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -9,10 +9,11 @@ export class CursoService {
 	constructor(){
 		this.cursos=[
 			new Curso('Curso de angular 15',15, 'finalizado', 'assets/img/angular.png'),
-			new Curso('Master en JavaScript',15, 'finalizado', 'assets/img/masterJavaScript.png'),
-			new Curso('Master en FrameWorks JavaScript',15, 'en curso', ''),
+			new Curso('Master en JavaScript',33, 'finalizado', 'assets/img/masterJavaScript.png'),
+			new Curso('Master en FrameWorks JavaScript',28, 'en curso', ''),
+      new Curso('Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap',21, 'en curso', ''),
+      }
 		]
-	}
 	getCursos(): Array<Curso>{
 		return this.cursos;
 	}
