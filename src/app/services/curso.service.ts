e37230b9b092959319e9b553683854f1a58ffff0
AlfonsoAguor <alfonsoaguor@gmail.com>
2024-08-27 12:27:44 +0200

    update cursos

--- src/app/services/curso.service.ts
+++ src/app/services/curso.service.ts
@@ -7,9 +7,10 @@ export class CursoService {
 
 	constructor(){
 		this.cursos = [
-			new Curso('Curso de angular 15', 15, 'finalizado', 'assets/img/angular.png'),
+			new Curso('Master en React: Aprender ReactJS, Hooks, MERN, NodeJS, JWT', 40, 'finalizado', 'assets/img/masterReact.webp'),
 			new Curso('Master en JavaScript: Aprender JS, jQuery, Angular, NodeJS', 33, 'finalizado', 'assets/img/masterJavaScript.png'),
-      new Curso('Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap', 21, 'finalizado', 'assets/img/mastercss.jpg'),
+			new Curso('Master en CSS: Responsive, SASS, Flexbox, Grid y Bootstrap', 21, 'finalizado', 'assets/img/mastercss.jpg'),
+			new Curso('Curso de angular 15', 15, 'finalizado', 'assets/img/angular.png'),
 			new Curso('Master en FrameWorks JavaScript', 28, 'en curso', '')
 		];
 	}
